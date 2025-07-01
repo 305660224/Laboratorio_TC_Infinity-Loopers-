20da55034fe73cfc1596b88ecfeb471c1d799703
Isaac <isaacchaves.uni@gmail.com>
2025-07-01 16:04:03 -0600

    se agrego interface
    se agrego el paquete lists con el interface list con add, remove, find, showall

---
+++ src/Lists/List.java
@@ -0,0 +1,22 @@
+/*
+ * Isaac chaves
+ */
+package Lists;
+
+public interface List <T> {
+    boolean add(T t); 
+    
+    boolean remove(T t); 
+    
+    T find(Object id);
+    
+    int showall();
+}
+
+
+
+
+
+
+
+
